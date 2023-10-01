b60fd154a1beaffd564b4e01bed20c471879b55e
Engbashayer <eng.almutairi1991@gmail.com>
2023-10-01 21:17:06 +0300

    products list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import products from "./products";
 
 function App() {
+  const productsList = products.map((book) => {
+    return (
+      <div className="card">
+        <h2>{book.name}</h2>
+        <h3>{book.price}</h3>
+        <img src={book.image} />
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <h1>My Book Store</h1>
-      <h4>this is the best book store</h4>
-      <img src="https://media.istockphoto.com/id/963186372/vector/flat-design-concept-online-books-store-hand-pick-book-from-internet-device-vector-illustrate.jpg?s=612x612&w=0&k=20&c=Actp37AZcbCtQm_mnFhpwBIYoHrYzrRMbY8iFFMjLIQ="></img>
-    </div>
+    <>
+      <div className="App">
+        <h1>Bashayer's bookstore</h1>
+
+        <img src="https://media.istockphoto.com/id/963186372/vector/flat-design-concept-online-books-store-hand-pick-book-from-internet-device-vector-illustrate.jpg?s=612x612&w=0&k=20&c=Actp37AZcbCtQm_mnFhpwBIYoHrYzrRMbY8iFFMjLIQ="></img>
+      </div>
+      <div className="productL">{productsList}</div>
+    </>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    name: "How to conntact with others",
+    price: 10,
+    image: "https://s3.amazonaws.com/jarir-files/images/original/14471.jpg",
+  },
+  {
+    name: "inner mind power",
+    price: 6,
+    image:
+      "https://kashkoolbooks.com/wp-content/uploads/2022/08/3D-%D9%82%D9%88%D8%A9-%D8%B9%D9%82%D9%84%D9%83-%D8%A7%D9%84%D8%A8%D8%A7%D8%B7%D9%86.png",
+  },
+  {
+    name: "the 5 AM club",
+    price: 20,
+    image:
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*YdGqkz_Z9NTgmMwpS0x_Vw.jpeg",
+  },
+  {
+    name: " how to introduce your self",
+    price: 15,
+    image: "https://www.storytel.com/images/640x640/0000580820.jpg",
+  },
+];
+
+export default products;
